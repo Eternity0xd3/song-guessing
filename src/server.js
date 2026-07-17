97c90b49821839f7dc2a3623006b7c9139e81d1c
Eternity0xd3 <eternity211@126.com>
2026-07-17 16:14:11 +0800

    deploy: ready for deployment

--- src/server.js
+++ src/server.js
@@ -2,12 +2,12 @@ import express from "express";
 import gameApiRouter from "./router/api.js";
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/api", gameApiRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
